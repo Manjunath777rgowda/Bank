cca59baf1b493d3d0acfa2e1875cf825844d4ca2
chinmayek <40786106+chinmayek@users.noreply.github.com>
2018-08-11 11:42:20 +0530

    Add files via upload

--- Validation.java
+++ Validation.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Validation {
+
     public int choiceValidation(String str) {
         try {
 
@@ -9,19 +13,20 @@ public class Validation {
         }
     }
 
-
     public boolean PhoneNumberValidation(String str) {
-        try {
-            int phoneNumber = Integer.parseInt(str);
-            int length = String.valueOf(str).length();
-            if (length == 10) {
+        try{
+            Pattern pattern=Pattern.compile("\\d{10}");
+            Matcher matcher=pattern.matcher(str);
+            if(matcher.matches()){
                 return true;
-            } else {
-                System.out.println("Phone number should be 10 digits");
+            }
+            else{
+                System.out.println("Mobile number should be 10 digits");
                 return false;
             }
-        } catch (Exception e) {
-            System.out.println("Invalid Phone Number");
+
+        }catch(Exception e){
+            System.out.println("Invalid mobile number!!!!") ;
             return false;
         }
     }
@@ -33,71 +38,81 @@ public class Validation {
         else return false;
     }
 
+    public boolean accNoValidation(String str){
+        if(str.matches("^[10]('SB'|'CA'|'RD'|'FD')[0-9][0-9][0-9][0-9]$")){
+            return true;
+        }
+        else return false;
+    }
+
     public boolean AdharNumberValidation(String str) {
-        try {
-            Long.parseLong(str);
-            int length = String.valueOf(str).length();
-            if (length == 12) {
+        try{
+            Pattern pattern=Pattern.compile("\\d{12}");
+            Matcher matcher=pattern.matcher(str);
+            if(matcher.matches()){
                 return true;
-            } else {
-                System.out.println("Adhar number should be 12 digits");
+            }
+            else{
+                System.out.println("adhar number should be 12 digits");
                 return false;
             }
-        } catch (Exception e) {
-            System.out.println("Invalid Adhar Number");
+
+        }catch(Exception e){
+            System.out.println("Invalid adhar number!!!!") ;
             return false;
         }
     }
 
     public boolean nameValidation(String str) {
-        if (str.matches("[a-zA-Z]+$")) {
-            return true;
+        try{
+            Pattern pattern=Pattern.compile("^[a-zA-Z][a-zA-Z-_\\.*\\s]{1,20}$");
+            Matcher matcher=pattern.matcher(str);
+            if(matcher.matches()){
+                return true;
+            }
+            else{
+                System.out.println("\n\nName did not meet conditions");
+                return false;
+            }
+
+        }catch(Exception e){
+            System.out.println("\n\nInvalid name!!!!") ;
+            return false;
         }
-        else return false;
     }
 
     public boolean dateValidation(String str)  {
-        if (str.matches("^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$")) {
+        if (str.matches("^\\d{2}\\-\\d{2}\\-\\d{4}$")) {
             String[] parts = str.split("-");
 
-            int year = Integer.parseInt(parts[0]);
+            int date = Integer.parseInt(parts[0]);
             int month = Integer.parseInt(parts[1]);
-            int date=Integer.parseInt(parts[2]);
+            int year=Integer.parseInt(parts[2]);
             if(year<1900 || year>=2018){
-              //  System.out.println("invalid year");
                 return false;
             }
             if(month<=0 || month>12)
             {
-               // System.out.println("invalid month");
                 return false;
             }
             if(date<0 || date>31 || (date>30 && (month==2 || month==4 || month==6 || month==9 || month==11)) || (date>29 && month==2) || (date>28 && month==2 && year%4!=0)){
-               // System.out.println("invalid date");
                 return false;
             }
-
-
             return true;
         }
         else return false;
 
     }
 
-
-
-
-
-
-        public boolean passwordValidation(String str)    {
-        if (str.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_]).{8,}$")) {
+    public boolean passwordValidation(String str)    {
+        if (str.matches(".{4,8}$")) {
             return true;
         }
         else return false;
     }
 
     public boolean panValidation(String str)    {
-        if (str.matches("[A-Z]{5}[0-9]{4}[A-Z]{1}")) {
+        if (str.matches("[A-Za-z]{5}[0-9]{4}[A-Za-z]{1}")) {
             return true;
         }
         else return false;
